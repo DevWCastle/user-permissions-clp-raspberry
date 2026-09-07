bd5eaefede25d0d855664eed59cc24bbbc347c34
DevWCastle <pedrocastelobrancodosantos@gmail.com>
2026-09-07 16:28:55 -0300

    Criando o modelo user e integrando no sqlite

---
+++ src/Models/User.ts
@@ -0,0 +1,27 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../db'; // Importa a conexão que você já criou
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false, // Não permite ficar em branco
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true, // Impede que dois usuários tenham o mesmo email
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+}, {
+    tableName: 'users' // Define o nome da tabela no banco de dados
+});
+
+export default User;
