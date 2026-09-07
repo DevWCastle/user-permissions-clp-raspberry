a8fd2b24b36edbdadc50354b1773638f125a77c0
DevWCastle <pedrocastelobrancodosantos@gmail.com>
2026-09-07 16:28:35 -0300

    inserindo a conexão com o banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
-import express, { Request, Response} from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
+import sequelize from "./db";
 
 const PORT = Number(process.env.PORT) || 4040;
-const HOSTNAME = process.env.HOSTNAME || 'localhost';
+const HOSTNAME = process.env.HOSTNAME || "localhost";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Banco de dados sincronizado com o Sequelize.");
+
+    app.listen(PORT, HOSTNAME, () => {
+      console.log(`Server running on http://${HOSTNAME}:${PORT}`);
+    });
+    
+  })
+  .catch((err) => {
+    console.error("Erro ao sincronizar com o banco:", err);
+  });
 
 app.get("/", (req: Request, res: Response) => {
-    
-    res.send("teset")
-
-})
-
-app.listen(PORT, HOSTNAME, () => {
-    console.log(`Server running on http://${HOSTNAME}:${PORT}`);
+  res.send("teset");
 });
