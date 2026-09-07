f8797219584d01876a682bfc9006f33952ace85e
DevWCastle <pedrocastelobrancodosantos@gmail.com>
2026-09-07 16:54:30 -0300

    criando função para testar a conexão com banco

---
+++ src/database/connectDatabase.ts
@@ -0,0 +1,18 @@
+import sequelize from "./db";
+
+// Função utilitária para testar a conexão com o banco
+export const connectDatabase = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('✅ Conexão com o SQLite estabelecida com sucesso.');
+        
+        // Sincroniza os modelos com o banco de dados (Cria as tabelas se não existirem)
+        // OBS: Em produção real, é recomendado usar Migrations em vez de .sync()
+        await sequelize.sync();
+        console.log('✅ Tabelas sincronizadas.');
+    } catch (error) {
+        console.error('❌ Não foi possível conectar ao banco de dados:', error);
+    }
+};
+
+
