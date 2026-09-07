466ea7632bdec1f186c74d87a0a89476b9121134
DevWCastle <pedrocastelobrancodosantos@gmail.com>
2026-09-07 16:55:03 -0300

    removendo função de conexão com banco e criando uma para testar conexão com o sequelize

--- src/database/db.ts
+++ src/database/db.ts
@@ -13,19 +13,14 @@ const sequelize = new Sequelize({
     logging: false, // Defina como `console.log` caso queira ver as queries SQL geradas no terminal
 });
 
-// Função utilitária para testar a conexão com o banco
-export const connectDatabase = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('✅ Conexão com o SQLite estabelecida com sucesso.');
-        
-        // Sincroniza os modelos com o banco de dados (Cria as tabelas se não existirem)
-        // OBS: Em produção real, é recomendado usar Migrations em vez de .sync()
-        await sequelize.sync();
-        console.log('✅ Tabelas sincronizadas.');
-    } catch (error) {
-        console.error('❌ Não foi possível conectar ao banco de dados:', error);
-    }
-};
+sequelize
+  .sync()
+  .then(() => {
+    console.log("✅ Banco de dados sincronizado com o Sequelize.");
+
+  })
+  .catch((err) => {
+    console.error("Erro ao sincronizar com o banco:", err);
+});
 
 export default sequelize;
