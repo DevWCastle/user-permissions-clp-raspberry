873efc73c2be477d2600a502c01becfc1e2fcc22
DevWCastle <pedrocastelobrancodosantos@gmail.com>
2026-09-10 14:04:19 -0300

    criando função de login para gerar o token jwt

--- src/Services/AuthService.ts
+++ src/Services/AuthService.ts
@@ -1,30 +1,37 @@
 import { time } from "node:console";
 import { UserRepository } from "../Repository/UserRepository";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
-export class AuthService{
-    private userRepository: UserRepository
+export class AuthService {
+  private userRepository: UserRepository;
 
-    constructor(userRepository: UserRepository){
-        this.userRepository = userRepository
-    }
+  constructor(userRepository: UserRepository) {
+    this.userRepository = userRepository;
+  }
 
-     cripto = () =>{
+  async cripto() {}
 
-    }
+  async login(email: string, password: string) {
+    const secretKey = process.env.SECRET_KEY;
 
-     login = async (email: string, password: string) => {
-    
-        const user = await this.userRepository.findByEmail(email)
+    const user = await this.userRepository.findByEmail(email);
 
-        if(!user) throw new Error('Usuário não encontrado.')
+    if (!user) throw new Error("E-mail ou senha inválidos.");
 
-        const userPassword = user.getDataValue('password') 
-        
-        if(userPassword != password) throw new Error('Senha incorreta.')
+    const userPassword = user.getDataValue("password");
 
-            
-        
+    if (userPassword != password) throw new Error("E-mail ou senha inválidos.");
 
-    }
+    const token = jwt.sign(
+      { id: user.id, access: user.access },
+      secretKey!,
+      {
+        expiresIn: "7 days",
+      },
+    );
 
+
+    return token
+  }
 }
