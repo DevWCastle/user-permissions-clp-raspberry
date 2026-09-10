877591dc5e66c231de297c256be4029f16decfa9
DevWCastle <pedrocastelobrancodosantos@gmail.com>
2026-09-10 14:08:27 -0300

    alterando interfaces para ter o nivel de acesso

--- src/Models/User/UserDTOS.ts
+++ src/Models/User/UserDTOS.ts
@@ -3,12 +3,14 @@ export interface CreateUserDTO {
   name: string;
   email: string;
   password: string;
+  access: number;
 }
 
 export interface UpdateUserDTO {
   name?: string;
   email?: string;
   password?: string;
+  access?: number;
 }
 
 
