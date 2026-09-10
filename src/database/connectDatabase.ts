79541ca5856987212ec1dc9d994cc3a7942bf9fe
DevWCastle <pedrocastelobrancodosantos@gmail.com>
2026-09-10 14:01:19 -0300

    permitindo alterar colunas em tabelas ja existentes

--- src/database/connectDatabase.ts
+++ src/database/connectDatabase.ts
@@ -8,7 +8,7 @@ export const connectDatabase = async () => {
         
         // Sincroniza os modelos com o banco de dados (Cria as tabelas se não existirem)
         // OBS: Em produção real, é recomendado usar Migrations em vez de .sync()
-        await sequelize.sync();
+        await sequelize.sync({alter: true});
         console.log('✅ Tabelas sincronizadas.');
     } catch (error) {
         console.error('❌ Não foi possível conectar ao banco de dados:', error);
