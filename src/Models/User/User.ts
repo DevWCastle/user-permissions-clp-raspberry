b65ccf915d5b44d2180e0240096b994d521e31d2
DevWCastle <pedrocastelobrancodosantos@gmail.com>
2026-09-10 14:03:46 -0300

    criando esquema novo para criação do user com sequelize

--- src/Models/User/User.ts
+++ src/Models/User/User.ts
@@ -1,27 +1,46 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../../database/db'; // Importa a conexão que você já criou
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../../database/db';
 
-const User = sequelize.define('User', {
+// 1. A classe declara os campos para o TypeScript reconhecer
+export class User extends Model {
+  public id!: number;
+  public name!: string;
+  public email!: string;
+  public password!: string;
+  public access!: number;
+}
+
+// 2. O User.init configura as tabelas no banco para o Sequelize
+User.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
     name: {
-        type: DataTypes.STRING,
-        allowNull: false, // Não permite ficar em branco
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true, // Impede que dois usuários tenham o mesmo email
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-}, {
-    tableName: 'users' // Define o nome da tabela no banco de dados
-});
+    access: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'users',
+  }
+);
 
 export default User;
